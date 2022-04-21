cc3f57380a3c20ea676f808b2d9ac69987127085
msamidag <msamidag@hotmail.com>
2022-04-21 10:58:39 +0300

    aritmetik islemler

---
+++ islemler.go
@@ -0,0 +1,20 @@
+package bukalemun
+
+var z float64
+
+func Toplama(x, y float64) float64 {
+	z = x + y
+	return z
+}
+func Cikarma(x, y float64) float64 {
+	z = x - y
+	return z
+}
+func Carpma(x, y float64) float64 {
+	z = x * y
+	return z
+}
+func Bolme(x, y float64) float64 {
+	z = x / y
+	return z
+}
